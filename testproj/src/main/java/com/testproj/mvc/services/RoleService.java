7c682857033b602b30383a3fffdfa1b7e12b5fc1
melnyknikolay <nikmlk87@gmail.com>
2016-06-21 12:48:19 +0300

    Create RoleService.java

--- testproj/src/main/java/com/testproj/mvc/services/RoleService.java
+++ testproj/src/main/java/com/testproj/mvc/services/RoleService.java
@@ -1,16 +1,15 @@
 package com.testproj.mvc.services;
 
-import com.testproj.mvc.model.Role;
-
 import java.util.List;
 
 /**
  * Created by DoctoRJurius on 16.06.16.
  */
-public interface RoleService {
-	void save(Role role);
+public interface Service<T> {
+	void save(T arg);
 	void delete(int id);
-	void update(Role role);
-	Role getById(int id);
-	List<Role> getRoles();
+	void update(T arg);
+	T getById(int id);
+        T getByName(String name);
+	List<T> getEntyties();
 }
